81c224c64c7853ddde951eed648f4ba8c9821ce5
Poorbi Dalal <poorbidalal2031@gmail.com>
2020-06-07 22:03:12 +0530

    Done

--- site/js/ajax-utils.js
+++ site/js/ajax-utils.js
@@ -22,11 +22,11 @@ function getRequestObject() {
 
 // Makes an Ajax GET request to 'requestUrl'
 ajaxUtils.sendGetRequest = 
-  function(requestUrl, responseHandler) {
+  function(requestUrl, responseHandler, isJasonResponse) {
     var request = getRequestObject();
     request.onreadystatechange = 
       function() { 
-        handleResponse(request, responseHandler); 
+        handleResponse(request, responseHandler, isJasonResponse); 
       };
     request.open("GET", requestUrl, true);
     request.send(null); // for POST only
@@ -36,11 +36,18 @@ ajaxUtils.sendGetRequest =
 // Only calls user provided 'responseHandler'
 // function if response is ready
 // and not an error
-function handleResponse(request,
-                        responseHandler) {
-  if ((request.readyState == 4) &&
-     (request.status == 200)) {
-    responseHandler(request);
+function handleResponse(request, responseHandler, isJasonResponse) {
+  if ((request.readyState == 4) && (request.status == 200)) {
+    if(isJasonResponse == undefined){
+      isJasonResponse = true
+    }
+
+    if(isJasonResponse){
+      responseHandler(JSON.parse(request.responseText))
+    }
+    else{
+      responseHandler(request.responseText);
+    }
   }
 }
 
--- site/js/script.js
+++ site/js/script.js
@@ -8,12 +8,21 @@ document.addEventListener("DOMContentLoaded",
         
         // Call server to get the name
         $ajaxUtils
-          .sendGetRequest("data/name.txt", 
-            function (request) {
-              var name = request.responseText;
+          .sendGetRequest("data/name.json", 
+            function (res) {
+              var message = res.firstName+ " "+ res.lastName
+              if(res.likesChineseFood){
+                message += "likes Chinese Food";
+              }
+              else{
+                message += "doesn't like Chinese Food";
+              }
+              message += " and uses ";
+              message += res.numberOfDisplays;
+              message += " displays for coding.";
 
               document.querySelector("#content")
-                .innerHTML = "<h2>Hello " + name + "!</h2>";
+                .innerHTML = "<h2>Hello " + message + "!</h2>";
             });
 
         
